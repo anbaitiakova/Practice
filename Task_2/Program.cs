be4013a7610c59bb52bdc3c4be362e483ac93f3d
anbaitiakova <71523513+anbaitiakova@users.noreply.github.com>
2021-07-06 21:56:55 +0300

    Add files via upload

---
+++ Task_2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace Task_2
+{
+    enum Sevirity
+    {
+        Trace,
+        Debug,
+        Information,
+        Warning,
+        Error,
+        Critical,
+        Severity
+    }
+    
+    class Program
+    { 
+        public sealed class Logger : IDisposable
+        {
+            private readonly StreamWriter _logWriter;
+            public Logger(string filePath)
+            {
+                _logWriter = new StreamWriter(filePath);
+            }
+
+            public void Log(string MessageForLog, Sevirity sevirity)
+            {
+                _logWriter.WriteLine($"[{DateTime.Now:G}][{sevirity}]: {MessageForLog}");
+            }
+            
+            public void Dispose()
+            {
+                _logWriter.Dispose();
+                GC.SuppressFinalize(this);
+            }
+
+            ~Logger()
+            {
+                _logWriter.Dispose();
+            }
+            
+        }
+        
+        
+        static void Main(string[] args)
+        {
+            using (Logger logger = new Logger(args[0]))
+            {
+                logger.Log("A lot of beer:)", Sevirity.Information);
+                logger.Log("Lab about logger", Sevirity.Error);
+                logger.Log("pupupu:3", Sevirity.Critical);
+                logger.Log("Something intersting", Sevirity.Debug);
+                logger.Log("Hello!", Sevirity.Trace);
+            }
+        }
+    }
+}
